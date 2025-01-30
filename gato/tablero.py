f761ffc31238fcf26e46d0bf1299597e4f819e44
verorodzn <a222215753@unison.mx>
2025-01-30 14:48:26 -0700

    Aregué lista de combinaciones y función para checar quién es el ganador checa_winner(), juego()

--- gato/tablero.py
+++ gato/tablero.py
@@ -42,6 +42,30 @@ def usuario(simbolos:dict):
             else:
                 print("Esa casilla ya está ocupada")
 
+def juego(simbolos:dict):
+    '''
+    Juego del gato
+    '''
+    lista_combinaciones = [
+        ["1", "2", "3"],
+        ["4", "5", "6"],
+        ["7", "8", "9"],
+        ["1", "4", "7"],
+        ["2", "5", "8"],
+        ["3", "6", "9"],
+        ["1", "5", "9"],
+        ["3", "5", "7"]
+    ]
+
+def checa_winner(simbolos:dict, combinaciones:list):
+    '''
+    Checa si hay un ganador
+    '''
+    for combinacion in combinaciones:
+        if simbolos[combinacion[0]] == simbolos[combinacion[1]] == simbolos[combinacion[2]]:
+            return simbolos[combinacion[0]]
+
+
 if __name__ == '__main__':
     numeros = [str(i) for i in range(1,10)]
     dsimbolos = {x:x for x in numeros}
