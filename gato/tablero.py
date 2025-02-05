c684ce621c8221fbc5818d1ba695840d9e3bc3cb
verorodzn <a222215753@unison.mx>
2025-02-05 14:37:20 -0700

    Se puede ganar, perder o quedar en empate

--- gato/tablero.py
+++ gato/tablero.py
@@ -57,6 +57,44 @@ def juego(simbolos:dict):
         ["3", "5", "7"]
     ]
 
+    en_juego = True
+
+    dibuja_tablero(simbolos)
+
+    movimientos = 0
+    gana = None
+
+    while en_juego:
+        usuario(simbolos)
+        dibuja_tablero(simbolos)
+        movimientos+= 1
+        gana = checa_winner(simbolos, lista_combinaciones)
+        
+        if gana is not None:
+            en_juego = False
+
+        if movimientos >= 9:
+            en_juego = False
+            continue
+
+        ia(simbolos)
+        dibuja_tablero(simbolos)
+        movimientos+= 1
+
+        gana = checa_winner(simbolos, lista_combinaciones)
+        
+        if gana is not None:
+            en_juego = False
+            continue
+
+        if movimientos >= 9:
+            en_juego = False
+            continue
+        
+    return gana
+
+
+
 def checa_winner(simbolos:dict, combinaciones:list):
     '''
     Checa si hay un ganador
@@ -64,13 +102,15 @@ def checa_winner(simbolos:dict, combinaciones:list):
     for combinacion in combinaciones:
         if simbolos[combinacion[0]] == simbolos[combinacion[1]] == simbolos[combinacion[2]]:
             return simbolos[combinacion[0]]
+    return None
 
 
 if __name__ == '__main__':
     numeros = [str(i) for i in range(1,10)]
     dsimbolos = {x:x for x in numeros}
-    dibuja_tablero(dsimbolos)
-    ia(dsimbolos)
-    dibuja_tablero(dsimbolos)
-    usuario(dsimbolos)
-    dibuja_tablero(dsimbolos)
+    g = juego(dsimbolos)
+
+    if g is not None:
+        print(f'El ganador es {g}')
+    else:
+        print('Empate')
