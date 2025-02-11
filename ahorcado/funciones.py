47ec96eae87bc6d27d8d7b7d874bf34b7f4c3503
verorodzn <a222215753@unison.mx>
2025-02-11 14:22:53 -0700

    Carga las plantillas y las despliega

--- ahorcado/funciones.py
+++ ahorcado/funciones.py
@@ -10,8 +10,32 @@ def carga_archivo_texto(archivo:str) -> list:
         contenido = file.readlines()
     return contenido
 
+def carga_plantillas(nombre_plantilla:str)-> dict:
+    '''
+    Carga las plantillas del juego a partir de un archivo de texto
+    '''
+    plantillas = {}
+    for i in range(6):
+        plantillas[i] = carga_archivo_texto(f"./ahorcado/plantillas/{nombre_plantilla}-{i}.txt")
+        return plantillas
+    
+def despliega_plantilla(diccionario:dict, nivel:int):
+    ''''
+    Despliega una plantilla del juego
+    '''
+
+    if nivel in diccionario:
+        template = diccionario[nivel]
+        for renglon in template:
+            print(renglon)
+
+
 
 if __name__ == "__main__":
-    lista = carga_archivo_texto("./plantillas/plantilla-0.txt")
-    for elemento in lista:
-        print(elemento)
+    plantillas = carga_plantillas("plantilla")
+    despliega_plantilla(plantillas, 5)
+    despliega_plantilla(plantillas, 4)
+    despliega_plantilla(plantillas, 3)
+    despliega_plantilla(plantillas, 2)
+    despliega_plantilla(plantillas, 1)
+    despliega_plantilla(plantillas, 0)
