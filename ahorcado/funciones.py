068bdd29fcabe125eb69c1cfd0abf18d98cc53f9
verorodzn <a222215753@unison.mx>
2025-02-11 14:51:33 -0700

    Obtiene palabras de un archivo y las despliega

--- ahorcado/funciones.py
+++ ahorcado/funciones.py
@@ -1,26 +1,25 @@
 '''
-Funciones auxiliares del juego Ahorcado
+funciones auxilaries para el juego del ahorcado
 '''
 
-def carga_archivo_texto(archivo:str) -> list:
-    ''''
-    Carga un archivo de texto y regresa una lista con el contenido del archivo
-    '''
-    with open(archivo, "r", encoding="utf-8") as file:
-        contenido = file.readlines()
-    return contenido
+def carga_archivo_texto(archivo:str)->list:
+    '''Carga un arcihivo de texto y devuelve una lista con las oraciones del archivo'''
 
-def carga_plantillas(nombre_plantilla:str)-> dict:
+    with open(archivo, 'r', encoding='utf-8') as f:
+        oraciones = f.readlines()
+    return oraciones
+
+def carga_plantillas(nombre_plantilla:str) -> dict:
     '''
-    Carga las plantillas del juego a partir de un archivo de texto
+    Carga las plantillas del juego a partir de un archivode texto
     '''
     plantillas = {}
     for i in range(6):
-        plantillas[i] = carga_archivo_texto(f"./ahorcado/plantillas/{nombre_plantilla}-{i}.txt")
-        return plantillas
-    
+        plantillas[i] = carga_archivo_texto(f'./ahorcado/plantillas/{nombre_plantilla}-{i}.txt')
+    return plantillas
+
 def despliega_plantilla(diccionario:dict, nivel:int):
-    ''''
+    '''
     Despliega una plantilla del juego
     '''
 
@@ -29,13 +28,19 @@ def despliega_plantilla(diccionario:dict, nivel:int):
         for renglon in template:
             print(renglon)
 
+def obten_palabras(lista:list)->list:
+    '''
+    Obtiene las palabras de un texto
+    '''
+    texto = ' '.join(lista[120:])
+    palabras = texto.split()
+    minusculas = [palabra.lower() for palabra in palabras]
+    set_palabras = set(minusculas)
+    return list(set_palabras)
 
-
-if __name__ == "__main__":
-    plantillas = carga_plantillas("plantilla")
+if __name__ == '__main__':
+    plantillas = carga_plantillas('plantilla')
     despliega_plantilla(plantillas, 5)
-    despliega_plantilla(plantillas, 4)
-    despliega_plantilla(plantillas, 3)
-    despliega_plantilla(plantillas, 2)
-    despliega_plantilla(plantillas, 1)
-    despliega_plantilla(plantillas, 0)
+    lista_oraciones = carga_archivo_texto('./ahorcado/datos/pg15532.txt')
+    lista_palabras = obten_palabras(lista_oraciones)
+    print(lista_palabras[:50])
